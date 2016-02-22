bc083579facecc1370c1e47c7c001ae8313e9940
xws <id512@126.com>
2016-02-22 18:53:29 +0800

    提交保存代码

--- socket/src/main/java/com/nitian/test/Test2.java
+++
@@ -1,9 +0,0 @@
-package com.nitian.test;
-
-public class Test2 {
-
-	public static void main(String[] args) {
-		;
-		System.out.println(Math.sqrt(3));
-	}
-}
---
+++ socket/src/main/java/com/nitian/test/UtilNIO.java
@@ -0,0 +1,30 @@
+package com.nitian.test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+public class UtilNIO {
+
+	public static void read(SelectionKey key) {
+
+		try {
+			// 服务器可读取消息:得到事件发生的Socket通道
+			SocketChannel channel = (SocketChannel) key.channel();
+			// 创建读取的缓冲区
+			ByteBuffer buffer = ByteBuffer.allocate(10);
+			channel.read(buffer);
+			byte[] data = buffer.array();
+			String msg = new String(data).trim();
+			System.out.println("服务端收到信息：" + msg);
+			ByteBuffer outBuffer = ByteBuffer.wrap(msg.getBytes());
+			channel.write(outBuffer);// 将消息回送给客户端
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
