0d7c9e426c7745f978df143b628fd9259895caf4
xws <id512@126.com>
2017-01-21 23:46:39 +0800

    client:去除错误代码

--- socket/src/main/java/com/nitian/socket/util/write/UtilWrite.java
+++ socket/src/main/java/com/nitian/socket/util/write/UtilWrite.java
@@ -1,13 +1,8 @@
 package com.nitian.socket.util.write;
 
 import com.nitian.socket.EngineSocket;
-import com.nitian.socket.core.CoreType;
-import com.nitian.socket.util.parse.UtilParseHttpWrite;
 import com.nitian.util.log.LogManager;
-import com.nitian.util.log.LogType;
 
-import java.io.IOException;
-import java.net.Socket;
 import java.util.Map;
 
 /**
