fb3985a9df0162c1c1c5ef83d8f94cce6d7bbb0f
admin <id512@126.com>
2017-07-23 01:52:50 -0400

    find job start 46%:redis from localhost

--- src/main/java/com/nitian/handler/redis/Redis.java
+++ src/main/java/com/nitian/handler/redis/Redis.java
@@ -8,7 +8,7 @@ import redis.clients.jedis.Jedis;
  */
 public class Redis {
 
-    private Jedis jedis = new Jedis("www.1036225283.com", 6379);
+    private Jedis jedis = new Jedis("127.0.0.1", 6379);
 
     private static Redis instance = new Redis();
 
